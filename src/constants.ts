1d43e17585db68b2fbb225fd754bf89b264ab2bd
Francisco Damião Terto <francisco.tsilva@hsl.org.br>
2025-05-02 18:00:23 -0300

    feat: create constants to default  options values

---
+++ src/constants.ts
@@ -0,0 +1,10 @@
+export const RL_KEY_PREFIX = 'bode-rl'
+export const RL_DEFAULT_OPTIONS = {
+	max: 100,
+	window: 60 * 1000,
+	mussurana: {
+		maxMemory: 10 * 1024 * 1024,
+		maxItems: 1000,
+		checkPeriod: 1000,
+	},
+}
