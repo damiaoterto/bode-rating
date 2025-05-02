7dd67cacaed135ef0cc6e24caa925e9e1ad90e23
Francisco Damião Terto <francisco.tsilva@hsl.org.br>
2025-05-02 14:57:52 -0300

    chore: add vitest and v8 coverage provider

---
+++ vitest.config.ts
@@ -0,0 +1,11 @@
+/// <reference types="vitest/config" />
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+  test: {
+    coverage: {
+			provider: 'v8',
+			reporter: ['text', 'html'],
+    },
+  },
+})
