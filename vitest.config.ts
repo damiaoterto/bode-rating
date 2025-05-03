fa0b5dc5556447ce60ddcb9a5af325326b2af5f1
Francisco Damião Terto <francisco.tsilva@hsl.org.br>
2025-05-03 11:17:28 -0300

    chore: add coverage pr comment

--- vitest.config.ts
+++ vitest.config.ts
@@ -7,7 +7,7 @@ export default defineConfig({
 	test: {
 		coverage: {
 			provider: 'v8',
-			reporter: ['clover', 'html'],
+			reporter: ['clover', 'html', 'lcov'],
 			exclude: [
 				...configDefaults.exclude,
 				'**/node_modules/**',
